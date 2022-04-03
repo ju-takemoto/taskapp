85357c367e26944d5939bfebdcd8e31b9a961c8a
ju-takemoto <ju-takemoto@kddi-tech.com>
2022-04-03 18:28:43 +0900

    error発生。

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by ktec on 2022/03/28.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // カテゴリ
+    @objc dynamic var category = ""
+    
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
